da948c49a32339476fc1b16a97dcb04a5fa05b03
Nenad-Dragon <nenad@dragon.com>
2023-12-13 18:32:20 +0900

    IPoolExtension contract added

---
+++ chiba-staking-contractV2/IPoolExtension.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+interface IPoolExtension {
+  function setShare(
+    uint256 pid,
+    address wallet,
+    uint256 balanceChange,
+    bool isRemoving
+  ) external;
+}
